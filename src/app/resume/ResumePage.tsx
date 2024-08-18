8c5098cf3c67c8d12b90445ea512694e46a2efe2
Olivie Bergeron <111806530+OlivTheOlive@users.noreply.github.com>
2024-08-18 13:02:55 -0400

    finished resume

--- src/app/resume/ResumePage.tsx
+++ src/app/resume/ResumePage.tsx
@@ -7,37 +7,31 @@ import "@react-pdf-viewer/default-layout/lib/styles/index.css";
 
 export default function ResumePage() {
   const defaultLayoutPluginInstance = defaultLayoutPlugin();
-
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen p-4 text-white">
-      <div className="max-w-6xl w-full">
-        <div className="flex flex-col lg:flex-row justify-between items-start">
-          <div className="lg:w-1/2 mb-8 lg:mb-0 p-4">
-            <h1 className="text-4xl font-bold mb-4">Resume</h1>
-            <div className="mt-4 bg-gray-800 p-4">
-              {/* Wrap your Viewer component inside the Worker component */}
-              <Worker
-                workerUrl={`https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js`}
-              >
-                <Viewer
-                  fileUrl="/Olivie_Bergeron_resume.pdf"
-                  plugins={[defaultLayoutPluginInstance]}
-                />
-              </Worker>
-            </div>
-            {/* Download Button */}
-            <a
-              href="/Olivie_Bergeron_resume.pdf"
-              target="_blank"
-              rel="noopener noreferrer"
-              download="Olivie_Bergeron_resume.pdf"
-            >
-              <button className="mt-4 bg-blue-600 text-white px-4 py-2 rounded flex items-center">
-                Download Resume
-              </button>
-            </a>
-          </div>
+    <div className="flex flex-col items-center justify-center min-h-screen p-4 text-white ">
+      <div className="max-w-4xl w-full text-center">
+        <h1 className="text-4xl font-bold mb-8">Resume</h1>
+        <div className=" p-4 mb-8 rounded">
+          <Worker
+            workerUrl={`https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js`}
+          >
+            <Viewer
+              fileUrl="/Olivie_Bergeron_resume.pdf"
+              plugins={[defaultLayoutPluginInstance]}
+            />
+          </Worker>
         </div>
+        <a
+          href="/Olivie_Bergeron_resume.pdf"
+          target="_blank"
+          rel="noopener noreferrer"
+          download="Olivie_Bergeron_resume.pdf"
+          className="flex items-center justify-center"
+        >
+          <button className="bg-blue-600 text-white px-6 py-3 rounded-lg shadow-lg ">
+            Download Resume
+          </button>
+        </a>
       </div>
     </div>
   );
