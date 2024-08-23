80cbcba30850459abd449cbf34e332b3ec857623
Olivie Bergeron <111806530+OlivTheOlive@users.noreply.github.com>
2024-08-22 20:03:06 -0400

    added project layout

---
+++ src/app/components/accordionComponent/index.tsx
@@ -0,0 +1,143 @@
+import React from "react";
+import {
+  Accordion,
+  AccordionHeader,
+  AccordionBody,
+  Typography,
+  Button,
+  IconButton,
+  Tooltip,
+} from "@material-tailwind/react";
+
+// Define the project type interface
+interface Project {
+  id: number;
+  title: string;
+  description: string;
+  date: string;
+  urls: Array<{ title: string; url: string }>; // Array of URLs with title and url
+}
+
+interface AccordionComponentProps {
+  projects: Array<Project>;
+}
+
+function Icon({ id, open }: { id: number; open: number | null }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={2}
+      stroke="currentColor"
+      className={`${
+        id === open ? "rotate-180" : ""
+      } h-5 w-5 transition-transform`}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+      />
+    </svg>
+  );
+}
+
+export function AccordionComponent({ projects }: AccordionComponentProps) {
+  const [open, setOpen] = React.useState<number | null>(null);
+
+  const handleOpen = (value: number) => setOpen(open === value ? null : value);
+
+  return (
+    <>
+      {projects.map((project) => (
+        <Accordion
+          key={project.id}
+          open={open === project.id}
+          animate={{
+            mount: { scale: 1 },
+            unmount: { scale: 0.9 },
+          }}
+          placeholder={undefined}
+          onPointerEnterCapture={undefined}
+          onPointerLeaveCapture={undefined}
+        >
+          <AccordionHeader
+            onClick={() => handleOpen(project.id)}
+            placeholder={undefined}
+            onPointerEnterCapture={undefined}
+            onPointerLeaveCapture={undefined}
+          >
+            <div className="grid grid-cols-[60%_35%_5%] items-center w-full">
+              <Typography
+                placeholder={undefined}
+                onPointerEnterCapture={undefined}
+                onPointerLeaveCapture={undefined}
+              >
+                {project.title}
+              </Typography>
+              <Typography
+                className="text-right"
+                placeholder={undefined}
+                onPointerEnterCapture={undefined}
+                onPointerLeaveCapture={undefined}
+              >
+                {project.date}
+              </Typography>
+              <div className="flex justify-end">
+                <Icon id={project.id} open={open} />
+              </div>
+            </div>
+          </AccordionHeader>
+          <AccordionBody>
+            <Typography
+              className="mb-4"
+              placeholder={undefined}
+              onPointerEnterCapture={undefined}
+              onPointerLeaveCapture={undefined}
+            >
+              {project.description}
+            </Typography>
+            <div className="flex gap-4">
+              {project.urls.map((link, index) => (
+                <Tooltip
+                  placement="bottom"
+                  animate={{
+                    mount: { scale: 1, y: 0 },
+                    unmount: { scale: 0, y: -25 },
+                  }}
+                  content={
+                    <div className="w-auto">
+                      <Typography
+                        color="white"
+                        className="font-medium"
+                        placeholder={undefined}
+                        onPointerEnterCapture={undefined}
+                        onPointerLeaveCapture={undefined}
+                      >
+                        {link.title}
+                      </Typography>
+                    </div>
+                  }
+                >
+                  <a href={link.url}>
+                    <IconButton
+                      color="black"
+                      size="lg"
+                      key={index}
+                      placeholder={undefined}
+                      onPointerEnterCapture={undefined}
+                      onPointerLeaveCapture={undefined}
+                    >
+                      <i className="fa-brands fa-github" />
+                    </IconButton>
+                  </a>
+                </Tooltip>
+              ))}
+            </div>
+          </AccordionBody>
+        </Accordion>
+      ))}
+    </>
+  );
+}
--- src/app/projects/ProjectsPage.tsx
+++ src/app/projects/ProjectsPage.tsx
@@ -3,11 +3,54 @@ import React from "react";
 import { Typography } from "@material-tailwind/react";
 import ProjectComponent from "../components/projectComponent";
 import { CarouselComponent } from "../components/carouselComponent";
+import { AccordionComponent } from "../components/accordionComponent";
 
 export default function ProjectPage() {
+  const project = [
+    {
+      id: 1,
+      title: "Zoho Customer Portal Prototype",
+      description: "test",
+      date: "July-Aug 2024",
+      urls: [
+        {
+          title: "BackEnd",
+          url: "https://github.com/OlivTheOlive/RoadRunnerV2",
+        },
+      ],
+    },
+    {
+      id: 2,
+      title: "RoadRunner",
+      description: "test",
+      date: "June-Aug 2024",
+      urls: [
+        {
+          title: "BackEnd",
+          url: "https://github.com/OlivTheOlive/RoadRunnerV2",
+        },
+      ],
+    },
+    {
+      id: 3,
+      title: "CSV editor",
+      description: "test",
+      date: " May-Aug 2024",
+      urls: [
+        {
+          title: "FrontEnd",
+          url: "https://github.com/OlivTheOlive/csvEditorFrontend",
+        },
+        {
+          title: "BackEnd",
+          url: "https://github.com/OlivTheOlive/csvEditorBackend",
+        },
+      ],
+    },
+  ];
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-4 text-white">
-      {/* Page Heading */}
       <Typography
         variant="h4"
         className="text-4xl font-bold mb-12 text-center"
@@ -17,45 +60,9 @@ export default function ProjectPage() {
       >
         Projects
       </Typography>
-      <CarouselComponent />
-      {/* Projects Grid */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 max-w-6xl w-full">
-        <ProjectComponent
-          time="June 2024"
-          img="https://example.com/project-image.jpg"
-          name="My Awesome Project"
-          description="This project involves building a full-stack application with React and Node.js."
-        />
-        <ProjectComponent
-          time="April 2024"
-          img="https://example.com/another-project.jpg"
-          name="Another Cool Project"
-          description="This project showcases a beautiful portfolio website built with Next.js."
-        />
-        <ProjectComponent
-          time="April 2024"
-          img="https://example.com/another-project.jpg"
-          name="Another Cool Project"
-          description="This project showcases a beautiful portfolio website built with Next.js."
-        />
-        <ProjectComponent
-          time="April 2024"
-          img="https://example.com/another-project.jpg"
-          name="Another Cool Project"
-          description="This project showcases a beautiful portfolio website built with Next.js."
-        />
-        <ProjectComponent
-          time="April 2024"
-          img="https://example.com/another-project.jpg"
-          name="Another Cool Project"
-          description="This project showcases a beautiful portfolio website built with Next.js."
-        />
-        <ProjectComponent
-          time="April 2024"
-          img="https://example.com/another-project.jpg"
-          name="Another Cool Project"
-          description="This project showcases a beautiful portfolio website built with Next.js."
-        />
+
+      <div className="w-9/12">
+        <AccordionComponent projects={project} />
       </div>
     </div>
   );
