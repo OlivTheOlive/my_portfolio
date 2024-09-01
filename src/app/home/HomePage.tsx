fb0761b40c7c4eafd8d7c7db11c65c1e92b86ca8
Olivie Bergeron <111806530+OlivTheOlive@users.noreply.github.com>
2024-09-01 17:58:15 -0400

    fixed the homepage

--- src/app/home/HomePage.tsx
+++ src/app/home/HomePage.tsx
@@ -17,7 +17,7 @@ const Lottie = dynamic(() => import("lottie-react"), { ssr: false });
 
 const HomePage: React.FC = () => {
   const [isClient, setIsClient] = useState(false);
-  const [open, setOpen] = useState(1);
+  const [open, setOpen] = useState(false);
 
   const [aboutRef, isInView] = useInView({ threshold: 0.1 });
 
@@ -27,104 +27,100 @@ const HomePage: React.FC = () => {
 
   useEffect(() => {
     if (isInView) {
-      setOpen(1);
+      setOpen(true);
     }
   }, [isInView]);
 
-  const handleAccordionOpen = (value: number) => {
-    setOpen(open === value ? 0 : value);
+  const handleAccordionOpen = (value: boolean) => {
+    setOpen(open === value ? false : value);
   };
 
   return (
-    <div>
-      <div className="grid grid-cols-1 md:grid-cols-2 items-center justify-center min-h-screen px-4 md:px-20 gap-4">
-        <div className="flex flex-col justify-center items-center md:items-end p-4 md:p-0 space-y-6">
-          <div className="space-y-4 text-center md:text-left">
-            <h1 className="text-4xl font-bold">
-              <HoverText text="Hello There!" />
-            </h1>
-            <h1 className="text-4xl font-bold">
-              I'm{" "}
-              <b>
-                <HoverText text="Olivie Bergeron" />
-              </b>
-            </h1>
-            <h3 className="text-lg font-semibold mt-4">
-              <TypewriterComponent
-                options={{
-                  strings: [
-                    "Software Developer",
-                    "MERN Stack Developer",
-                    "Canadian Frenchman",
-                    "Beagles Are Loud",
-                  ],
-                  autoStart: true,
-                  loop: true,
-                  delay: 70,
-                  deleteSpeed: 20,
-                }}
-              />
-            </h3>
-          </div>
+    <div className="min-h-screen px-4 md:px-20 py-8 space-y-40">
+      <section
+        id="home"
+        className="min-h-screen flex flex-col justify-center items-center space-y-8"
+      >
+        <div className="text-center space-y-4">
+          <h1 className="text-4xl font-bold">
+            <HoverText text="Hello There!" />
+          </h1>
+          <h1 className="text-4xl font-bold">
+            I'm{" "}
+            <b>
+              <HoverText text="Olivie Bergeron" />
+            </b>
+          </h1>
+          <h3 className="text-lg font-semibold mt-4">
+            <TypewriterComponent
+              options={{
+                strings: [
+                  "Software Developer",
+                  "MERN Stack Developer",
+                  "Canadian Frenchman",
+                  "Beagles Are Loud",
+                ],
+                autoStart: true,
+                loop: true,
+                delay: 70,
+                deleteSpeed: 20,
+              }}
+            />
+          </h3>
         </div>
-        {isClient && (
-          <div className="flex justify-center p-2 md:p-0 md:justify-start">
-            <Tilt perspective={500} scale={1.2}>
-              <Lottie
-                className="w-64 h-64 md:w-80 md:h-80 animate-[rotateLoop_5s_infinite]"
-                animationData={Rocket}
-                loop={true}
-              />
-            </Tilt>
-          </div>
-        )}
-      </div>
-
-      <div className="grid grid-cols-1 md:grid-cols-2 items-center justify-center mt-64 px-8 md:px-20 gap-4">
-        <div className="flex justify-center p-4">
-          <WaterDropGrid />
+        <div className="flex justify-center">
+          <Tilt perspective={500} scale={1.2}>
+            <Lottie
+              className="w-64 h-64 md:w-80 md:h-80 animate-[rotateLoop_5s_infinite]"
+              animationData={Rocket}
+              loop={true}
+            />
+          </Tilt>
         </div>
-        <div
-          className="w-full max-w-2xl mx-auto text-center md:text-left"
-          ref={aboutRef}
-        >
+      </section>
+      <section
+        id="about"
+        className="min-h-screen flex flex-col justify-center items-center space-y-8"
+      >
+        <div ref={aboutRef} className="w-full max-w-lg">
           <Accordion
-            open={open === 1}
-            className="mt-2"
+            open={open}
+            className="w-full"
             placeholder={undefined}
             onPointerEnterCapture={undefined}
             onPointerLeaveCapture={undefined}
           >
             <AccordionHeader
-              onClick={() => handleAccordionOpen(1)}
+              onClick={() => handleAccordionOpen(true)}
               placeholder={undefined}
               onPointerEnterCapture={undefined}
               onPointerLeaveCapture={undefined}
             >
-              <HoverText text="Who Am I??" />
+              <HoverText text="Who Am I???" />
             </AccordionHeader>
             <AccordionBody>
-              <div className="max-h-[500px] overflow-auto space-y-2">
-                <p className="text-base font-medium leading-7 tracking-wide text-justify text-white">
-                  I love the process of changing a raw idea into a website or a
-                  product that impacts lives. I want to do work that challenges
-                  me as a developer & work that I can be proud of.
-                  <br />
-                  <br />I have a solid foundation in <b>
-                    Java and JavaScript
-                  </b>{" "}
-                  and know a bit of <b>Python</b> and am working on a few
-                  projects in the <b>MERN</b> stack.
-                  <br />I am currently learning <b>Next.js</b> and{" "}
-                  <b>Typescript</b>.
-                  <br />
-                  <br />
+              <div className="max-h-[500px] overflow-auto space-y-4 text-base font-medium leading-7 tracking-wide text-justify text-white">
+                <p>
+                  I'm passionate about transforming raw ideas into meaningful
+                  products that make a difference in people's lives. I seek work
+                  that pushes my boundaries as a developer and allows me to
+                  create something I can truly be proud of.
+                </p>
+                <p>
+                  My skills include a strong foundation in <b>Java</b> and{" "}
+                  <b>JavaScript</b>, with working knowledge of <b>Python</b>.
+                  I'm actively involved in projects using the <b>MERN</b> stack
+                  and currently expanding my expertise in <b>Next.js</b>,{" "}
+                  <b>TypeScript</b>, and <b>C#</b>.
                 </p>
               </div>
             </AccordionBody>
           </Accordion>
         </div>
-      </div>
+        <div className="flex justify-center">
+          <WaterDropGrid />
+        </div>
+      </section>
     </div>
   );
 };
