e30a1d31a955d7f55b32b980542fe1b0190047b4
Olivie Bergeron <111806530+OlivTheOlive@users.noreply.github.com>
2024-09-02 18:31:43 -0400

    changed the sun to be a lottie animation that moves from left to right upon scrolling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,11 @@ import FooterComponent from "./components/footer";
 import { GridLoader } from "react-spinners";
 import { useEffect, useState } from "react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import dynamic from "next/dynamic";
+import Sun from "@/app/lottieAnimation/sun.json";
 
+// Dynamically import Lottie
+const Lottie = dynamic(() => import("lottie-react"), { ssr: false });
 const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
@@ -28,6 +32,29 @@ export default function RootLayout({
     return () => clearTimeout(timer);
   }, []);
 
+  // Sun Animation Movement
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPositionY = window.scrollY;
+      const documentHeight =
+        document.documentElement.scrollHeight - window.innerHeight;
+      const windowWidth = window.innerWidth;
+
+      const scrollPercentage = scrollPositionY / documentHeight;
+      const sunPositionX = scrollPercentage * (windowWidth - 150);
+
+      const sunElement = document.getElementById("sun-animation");
+      if (sunElement) {
+        sunElement.style.transform = `translate(${sunPositionX}px, 0px)`;
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    // Cleanup event listener on component unmount
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
     <html lang="en">
       <head>
@@ -39,6 +66,7 @@ export default function RootLayout({
           referrerPolicy="no-referrer"
         />
       </head>
+
       <body className={inter.className}>
         <SpeedInsights />
         {loading ? (
@@ -54,7 +82,27 @@ export default function RootLayout({
         ) : (
           <div>
             <NavbarComponent />
-            {children}
+            {/* Sun Animation in Background */}
+            <div
+              id="sun-animation"
+              className="fixed top-10 left-0"
+              style={{
+                width: "150px",
+                height: "150px",
+                zIndex: -1, // Put the sun behind the content
+                pointerEvents: "none", // Prevent the sun from capturing mouse events
+              }}
+            >
+              <Lottie animationData={Sun} loop={true} />
+            </div>
+            <div
+              style={{
+                position: "relative", // Ensure the content is above the sun
+                zIndex: 1, // Higher z-index than the sun
+              }}
+            >
+              {children}
+            </div>
             <FooterComponent />
           </div>
         )}
